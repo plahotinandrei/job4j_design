71aad2f3f113c67287ca2d1defcaabc584894416
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-05-24 21:31:54 +0300

    0.2. FileInputStream. fix

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
@@ -22,7 +23,7 @@ public class EvenNumberFile {
                     System.out.println(number + " - не число");
                 }
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
