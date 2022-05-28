d7a29f89a8c6b51444d4c3283df4f754747b9ccc
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-05-28 15:25:00 +0300

    5.1. Именованные аргументы.

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException(String.format("Key %s not found", key));
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String arg:args) {
+            strValid(arg);
+            String[] kv = arg.split("=", 2);
+            values.put(kv[0].substring(1), kv[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    private void strValid(String str) {
+        String[] kv = str.split("=", 2);
+        if (kv.length < 2 || !kv[0].startsWith("-") || kv[0].length() < 2 || kv[1].length() == 0) {
+            throw new IllegalArgumentException("One of the arguments does not match the pattern -key=value");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
