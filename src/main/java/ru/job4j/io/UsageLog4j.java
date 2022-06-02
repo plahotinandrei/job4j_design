d98f04b2b1355875432d393de43cfb10512b33e3
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-06-02 22:19:17 +0300

    1. Log4j. Логирование системы. fix

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,6 +1,5 @@
 package ru.job4j.io;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -9,7 +8,6 @@ public class UsageLog4j {
     private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
         LOG.trace("trace message");
         LOG.debug("debug message");
         LOG.info("info message");
