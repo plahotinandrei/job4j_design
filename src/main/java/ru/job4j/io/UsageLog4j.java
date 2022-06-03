8bf331513f97dce2810bcc42562be0df7261becf
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-06-03 12:31:34 +0300

    3. Slf4j - вывод переменных

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,20 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Andrey Plakhotin";
+        int age = 27;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        byte n1 = 10;
+        short n2 = 20;
+        int n3 = 30;
+        long n4 = 40;
+        float n5 = 50.1F;
+        double n6 = 60.2D;
+        LOG.debug("numbers - byte: {}, short: {}, int: {}, long: {}, float: {}, double: {}", n1, n2, n3, n4, n5, n6);
+        boolean t = true;
+        boolean f = false;
+        LOG.debug("boolean: {} and {}", t, f);
+        char c = 'A';
+        LOG.debug("char: {}", c);
     }
 }
