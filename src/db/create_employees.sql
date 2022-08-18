2c09ece39d047eb8aee7fc8827ee67ab655c8f7f
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-08-18 18:05:16 +0300

    0. Что такое база данных. fix

--- src/db/create_employees.sql
+++
@@ -1,7 +0,0 @@
-create table employees(
-    id serial primary key,
-    firstname varchar(255),
-    lastname varchar(255),
-    age integer,
-    position text
-);
--- src/db/scripts_employees.sql
+++ src/db/scripts_employees.sql
@@ -1,7 +1,15 @@
+create table employees(
+    id serial primary key,
+    firstname varchar(255),
+    lastname varchar(255),
+    age integer,
+    position text
+);
+
 insert into employees(firstname, lastname, age, position) values('Андрей', 'Плахотин', 28, 'Java developer');
 
-select * from employees
+select * from employees;
 
-update employees set position = 'Junior java developer'
+update employees set position = 'Junior java developer';
 
-delete from employees
+delete from employees;
