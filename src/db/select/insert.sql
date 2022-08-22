0c6b29df9262db0bc7584891830f061d4b247eab
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-08-22 17:02:56 +0300

    0. Select

---
+++ src/db/select/insert.sql
@@ -0,0 +1,9 @@
+insert into fauna(name, avg_age, discovery_date) values ('Canada goose', 4000, '1983-06-24');
+insert into fauna(name, avg_age, discovery_date) values ('Squirrel, pine', 8000, '1985-03-26');
+insert into fauna(name, avg_age, discovery_date) values ('Eurasian beaver', 25000, '2010-05-24');
+insert into fauna(name, avg_age, discovery_date) values ('Booby, masked', 13000, '1992-12-16');
+insert into fauna(name, avg_age, discovery_date) values ('African skink', 21000, '1998-09-22');
+insert into fauna(name, avg_age, discovery_date) values ('Swordfish, red-necked', 24000, '1926-08-20');
+insert into fauna(name, avg_age, discovery_date) values ('Kookaburra, laughing', 13000, '1971-09-18');
+insert into fauna(name, avg_age, discovery_date) values ('Asiatic wild ass', 9000, null);
+insert into fauna(name, avg_age, discovery_date) values ('Pintail, bahama', 9000, '1992-09-19');
