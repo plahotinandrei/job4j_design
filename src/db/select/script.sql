e5bc7a9b079ad5af2f56f0f4c62ab8741f25976a
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-08-23 10:49:16 +0300

    0. Select fix

--- src/db/select/script.sql
+++ src/db/select/script.sql
@@ -1,3 +1,10 @@
+create table fauna (
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+
 insert into fauna(name, avg_age, discovery_date) values ('Canada goose', 4000, '1983-06-24');
 insert into fauna(name, avg_age, discovery_date) values ('Squirrel, pine', 8000, '1985-03-26');
 insert into fauna(name, avg_age, discovery_date) values ('Eurasian beaver', 25000, '2010-05-24');
@@ -7,3 +14,8 @@ insert into fauna(name, avg_age, discovery_date) values ('Swordfish, red-necked'
 insert into fauna(name, avg_age, discovery_date) values ('Kookaburra, laughing', 13000, '1971-09-18');
 insert into fauna(name, avg_age, discovery_date) values ('Asiatic wild ass', 9000, null);
 insert into fauna(name, avg_age, discovery_date) values ('Pintail, bahama', 9000, '1992-09-19');
+
+select * from fauna where name like '%fish%';
+select * from fauna where avg_age >=10000 and avg_age <= 20000;
+select * from fauna where discovery_date is null;
+select * from fauna where discovery_date < '01.01.1950';
