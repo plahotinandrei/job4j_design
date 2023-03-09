61bea8c301cb1c70a045f9730285274e9098b841
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2023-03-10 00:53:14 +0300

    4. Найти утечку памяти.

---
+++ src/main/java/ru/job4j/gc/leak/Comment.java
@@ -0,0 +1,22 @@
+package ru.job4j.gc.leak;
+
+public class Comment {
+
+    private String text;
+
+    private User user;
+
+    public Comment(String text, User user) {
+        this.text = text;
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{"
+                + "text='"
+                + text + '\''
+                + ", user=" + user
+                + '}';
+    }
+}
